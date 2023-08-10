f731ca406c1eabdceff69caa60cfd9757067bdf6
facudam <facundo89caceres@gmail.com>
2023-08-10 20:45:20 -0300

    Creating MainLayout at layouts folder

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import { FC } from "react";
+
+interface MainLayoutProps {
+    children: JSX.Element,
+    title: string,
+    author: string,
+    description: string,
+}
+
+export const MainLayout: FC<MainLayoutProps> = ({ children, title, author, description }) => {
+    return(
+        <>
+            <Head>
+                <title>{ title }</title>
+                <meta name="author" content={ author } />
+                <meta name="description" content={ description } />
+            </Head>
+            {
+                children
+            }
+        </>
+    )
+}
