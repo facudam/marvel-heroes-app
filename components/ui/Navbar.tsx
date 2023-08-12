09a353b08fadba0060804b81e588849d0f715e5e
facudam <facundo89caceres@gmail.com>
2023-08-12 12:29:58 -0300

    Adding all fonts to use and creating logo and still designing nav

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link"
 import styles from '@/styles/Navbar.module.css'
+import Image from "next/image"
 
 export const Navbar = () => {
     return(
         <nav className={ styles.nav }>
-            <Link className={` ${styles.link}`} href=''>listas</Link>
-            <Link className={` ${styles.link} ${styles.logo}`} href=''>Marvel heroes app</Link>
-            <Link className={` ${styles.link}`} href=''>Favourites</Link>
+            <div className={ styles.navCntPrimary }>
+                <Link className={`${styles.link}`} href=''>listas</Link>
+                <Link className={`${styles.link} ${styles.logo}`} href=''>marvel heroes app</Link>
+                <Link className={`${styles.link}`} href=''>Favourites</Link>
+            </div>
+            <div className={ `${styles.navCntPrimary} ${styles.navCntSecondary}` }>
+                <Link className={`${styles.link}`} href=''>comics</Link>
+                <Link className={`${styles.link}`} href=''>characters</Link>
+                <Link className={`${styles.link}`} href=''>more</Link>
+            </div>
         </nav>
     )
 }
