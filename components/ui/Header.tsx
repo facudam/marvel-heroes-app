a40268ccc1b2b1715b380c00b59db637fd0edf9d
facudam <facundo89caceres@gmail.com>
2023-08-12 19:49:05 -0300

    Changing h1 animation

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,8 +4,8 @@ import styles from '@/styles/Header.module.css'
 export const Header: FC = () => {
     return(
         <header className={`${styles.header}`}>
-            <h1 className={`${styles.textShadowPopTop}`}>The Marvel Heroes Application</h1>
-            <p>Discover your gateway to explore a universe of iconic characters and captivating comics. Dive in, explore heroes, comics, and curate your favourites with ease.</p>
+            <h1 className={`${styles.textShadowPopLeft}`}>The Marvel Heroes Application</h1>
+            <p>Discover the gateway to explore a universe of iconic characters and captivating comics. Dive in, explore heroes, comics, and curate your favourites with ease.</p>
         </header>
     )
 }
