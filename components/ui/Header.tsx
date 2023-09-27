5b034434af89926cc0d8ab55863e3f03ac42ba0e
facudam <facundo89caceres@gmail.com>
2023-09-26 21:16:23 -0300

    Adding favourite button at header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -6,6 +6,9 @@ export const Header: FC = () => {
         <header className={`${styles.header}`}>
             <h1 className={`${styles.textShadowPopBottom}`}>The <span>Marvel Heroes</span> application</h1>
             <p>Discover the gateway to explore a universe of iconic characters and captivating comics. Dive in, explore heroes, comics, and curate your favourites with ease.</p>
+            <div className={ styles.btnContainer }>
+                <button className={ styles.button }>Favourites</button>
+            </div>
         </header>
     )
 }
